4d294ecb33d39a97d7a188dc3a199ce8eed4c46b
adammckerlie@gmail.com <adammckerlie@gmail.com>
2010-11-27 10:45:37 -0500

    URLS damn

--- urls.py
+++ urls.py
@@ -1,16 +1,14 @@
 from django.conf.urls.defaults import *
-
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
+from django.conf import settings
 
 urlpatterns = patterns('',
-    # Example:
-    # (r'^hackmtl/', include('hackmtl.foo.urls')),
+    (r'^', include('opentable.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
-    # (r'^admin/', include(admin.site.urls)),
+    (r'^admin/', include(admin.site.urls)),
+
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve', { 'document_root' : settings.MEDIA_ROOT }, name='media'),
 )
