ddf2741cd28ee4f38590928bdd8e6d44e50c987c
Terence Lo <tlo@gap-00254bd30096.local>
2010-11-27 15:21:05 -0500

    so far..

--- apps/opentable/models.py
+++ apps/opentable/models.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils.translation import ugettext_lazy as _
@@ -9,7 +9,12 @@ from libraries.cakemail import CakeRelay
 import json
 
 # this should be outside of the model but whatever
-EMAIL_TEMPLATE = (u"""Hello""")
+EMAIL_TEMPLATE = unicode("""Hello %(name)s,<br>
+This is a reminder that you have a reservation at %(restaurant)s on %(date)s.<br>
+If you're looking for something to do afterwards, here are some movies playing at %(theatre_name)s.<br>
+%(movies)<br>
+Thanks for using Ceviche. 
+                """)
 
 class Facility(models.Model):
     name = models.CharField(_('Name'), max_length=255)
@@ -52,7 +57,7 @@ class Restaurant(Facility):
             if not minDiff or diff < minDiff:
                 minDiff = diff
                 closestTheatre = theatre
-        return closestTheatre.name
+        return closestTheatre
 
     @models.permalink
     def get_absolute_url(self):
@@ -72,13 +77,26 @@ class Reservation(models.Model):
     notified = models.BooleanField(default=False)
 
     def notify_user(self):
-        
+        theatre = self.restaurant.get_closest_theatre() 
+        movies = theatre.get_recommended_movies(start_time = (self.reservation_time + timedelta(hours=1.5)).strftime("%H:%s"), limit=3)
+       
+        movieText = "" 
+        for movie in movies: 
+            movieText += movieText + "%s @ %s <br>" % (movie[0], movie[1])
+       
+        movieText = unicode(movieText) 
+        dict = {    'name': self.user.first_name,
+                    'restaurant': self.restaurant.name,
+                    'date': self.reservation_time.strftime("%A %B %d %Y at %H:%m"),
+                    'theatre_name': theatre.name,
+                    'movies': movieText, 
+                }
         # to define:
         params = {
             'user_key': settings.CAKEMAIL_USER_KEY,
             'email': self.user.email,
-            'html_message': EMAIL_TEMPLATE,
-            'subject': 'Your reservation at %s for: %s' % (self.restaurant.name, self.reservation_time.strftime("%H:%m")),
+            'html_message': unicode(EMAIL_TEMPLATE % (dict)),
+            'subject': 'Your reservation at %s for: %s' % (self.restaurant.name, self.reservation_time.strftime("%A %B %d %Y at %H:%m")),
             'sender_name': 'Ceviche Notifier',
             'sender_email': 'admin@ceviche.com',
         }
