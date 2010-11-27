ae2a0b0b2d77e6ad233ce4d533f68cd5b0b447c9
adammckerlie@gmail.com <adammckerlie@gmail.com>
2010-11-27 12:23:01 -0500

    Saving reservations now working

--- apps/opentable/views.py
+++ apps/opentable/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import get_object_or_404, render_to_response
 from django.http import Http404, HttpResponse, HttpResponseRedirect, HttpResponseForbidden
 from django.conf import settings
 from django.core.urlresolvers import reverse
+from django import forms
 from models import Restaurant
 from forms import ReservationForm
 
@@ -22,10 +23,37 @@ def reservation(request, restaurant_id=None):
     template_name = 'reservation.html'
     context = RequestContext(request)
     dict = {}
+    if restaurant_id:
+        dict['restaurant'] = Restaurant.objects.get(pk=restaurant_id)
     if request.POST:
-        pass
+        if request.POST.get('cancel'):
+            return HttpResponseRedirect(reverse('index'))
+        form = ReservationForm(request.POST)
+        if form.is_valid():
+            reservation = form.save(commit=False)
+            if request.user:
+                reservation.user = request.user
+            restaurant = reservation.restaurant
+            restaurant.num_reservations += 1
+            restaurant.lat = str(reservation.restaurant.lat)
+            restaurant.lon = str(reservation.restaurant.lon)
+            if restaurant.capacity - reservation.num_people < 0:
+                assert False, 'no mores room'
+            else:
+                restaurant.capacity -= reservation.num_people 
+            restaurant.save()
+            reservation.save()
+            dict['msg'] = 'Thank you for your reservation.'
+        else:
+            dict['form'] = form
+            if dict.get('restaurant'):
+                dict['form'].fields['restaurant'].widget = forms.HiddenInput()
     else:
-        dict['form'] = ReservationForm()
+        if dict.get('restaurant'):
+            dict['form'] = ReservationForm(initial={'restaurant':dict['restaurant']})
+            dict['form'].fields['restaurant'].widget = forms.HiddenInput()
+        else:
+            dict['form'] = ReservationForm()
     return render_to_response(
         template_name,
         dict,
