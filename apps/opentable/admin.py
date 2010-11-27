5754ff17a1ac5f731a1dc9e1076bcd51499a0760
adammckerlie@gmail.com <adammckerlie@gmail.com>
2010-11-27 14:20:57 -0500

    Theatres in Admin

--- apps/opentable/admin.py
+++ apps/opentable/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from models import Restaurant, Reservation
+from models import Restaurant, Reservation, Theatre
 
 class RestaurantAdmin(admin.ModelAdmin):
     pass
@@ -7,6 +7,10 @@ class RestaurantAdmin(admin.ModelAdmin):
 class ReservationAdmin(admin.ModelAdmin):
     pass
 
+class TheatreAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Theatre, TheatreAdmin)
 admin.site.register(Restaurant, RestaurantAdmin)
 admin.site.register(Reservation, ReservationAdmin)
 
