73f1183d46cb979dbafdbd76077b4cad8875cc18
adammckerlie@gmail.com <adammckerlie@gmail.com>
2010-11-27 13:52:42 -0500

    Google Maps integration, get_absolute_url for restaurants

--- apps/opentable/views.py
+++ apps/opentable/views.py
@@ -5,6 +5,7 @@ from django.http import Http404, HttpResponse, HttpResponseRedirect, HttpRespons
 from django.conf import settings
 from django.core.urlresolvers import reverse
 from django import forms
+from django.forms.util import ErrorList
 from models import Restaurant
 from forms import ReservationForm
 
@@ -13,6 +14,7 @@ def index(request):
     context = RequestContext(request)
     dict = {}
     dict['restaurants'] = Restaurant.objects.all().order_by('-num_reservations')[:8]
+    dict['MAPS_ID'] = settings.GOOGLE_MAPS_ID
     return render_to_response(
         template_name,
         dict,
@@ -38,7 +40,11 @@ def reservation(request, restaurant_id=None):
             restaurant.lat = str(reservation.restaurant.lat)
             restaurant.lon = str(reservation.restaurant.lon)
             if restaurant.capacity - reservation.num_people < 0:
-                assert False, 'no mores room'
+                form._errors['num_people'] = ErrorList([u"Unfortunately this restaurant doesn't like you and can't accomodate you.  Actually, they just are busy.  Try another time..."])
+                dict['form'] = form
+                if dict.get('restaurant'):
+                    dict['form'].fields['restaurant'].widget = forms.HiddenInput()
+                return render_to_response( template_name, dict, context, )
             else:
                 restaurant.capacity -= reservation.num_people 
             restaurant.save()
@@ -60,11 +66,15 @@ def reservation(request, restaurant_id=None):
         context,
     )
 
-def restaurants(request):
+def restaurants(request, restaurant_id=None):
     template_name = 'restaurants.html'
     context = RequestContext(request)
     dict = {}
-    dict['restaurants'] = Restaurant.objects.all()
+    if restaurant_id:
+        dict['restaurant'] = Restaurant.objects.get(pk=restaurant_id)
+        template_name = 'restaurant.html'
+    else:
+        dict['restaurants'] = Restaurant.objects.all().order_by('name')
     return render_to_response(
         template_name,
         dict,
